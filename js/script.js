ad4bffd91d8a0b11a45281c03dcad313f515dde7
pavliukOleh <berzevul@gmail.com>
2023-03-10 15:21:15 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// const varA = 27;
+// const varB = 42;
+
+// // ========== Початок зони редагування ===============
+
+// const sumTotal = varA+varB;
+// console.log(sumTotal);
+// const isVarAGreater = varA>varB;
+// console.log(isVarAGreater);
+// const centModulo = (varA*100)%varB;
+// console.log(centModulo);
+// const dualCondition = isVarAGreater && centModulo>=24;
+// console.log(dualCondition);
+// const concatToString = ""+varA+varB;
+// console.log(concatToString);
+// // ========== Кінець зони редагування ===============
+
+
+
